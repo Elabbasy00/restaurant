c89abaaaca594cd271bcdf46876dadd14c39614a
Elabbasy00 <ahmedelabbasy5@gmail.com>
2025-08-09 02:00:27 +0300

    aa

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,19 +2,22 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-// import path from "path";
+import path from "path";
 export default defineConfig({
   base: "/",
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   build: {
     manifest: true,
+    rollupOptions: {
+      preserveEntrySignatures: "strict",
+    },
   },
   server: {
     origin: "http://localhost", // Match your Nginx port
   },
-  // resolve: {
-  //   alias: {
-  //     // '~': path.resolve(__dirname, './app/frontend'), // Absolute path
-  //   },
-  // },
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./app"), // Must match tsconfig's "paths"
+    },
+  },
 });
